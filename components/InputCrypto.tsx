74318b78d44f7cb2b3052480361d8603caf3b6de
mervartl <lukas-mervart@seznam.cz>
2022-11-02 21:56:47 +0100

    cena + odhlasenej uzivatel funguje

--- components/InputCrypto.tsx
+++ components/InputCrypto.tsx
@@ -3,6 +3,7 @@ import { collection, onSnapshot, orderBy, query } from "@firebase/firestore";
 import { db } from "../firebase";
 import { useUserContext } from "./userContext";
 import axios from "axios";
+import { Typography } from "@mui/material";
 
 export const InputCrypto: React.FC = () => {
 
@@ -11,7 +12,7 @@ export const InputCrypto: React.FC = () => {
     const [data, setData] = useState([]);
 
     const url =
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=czk&order=market_cap_desc&per_page=250&page=1&sparkline=false';
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=czk&order=market_cap_desc&per_page=200&page=1&sparkline=false';
 
     useEffect(() => {
         axios.get(url).then((response) => {
@@ -19,7 +20,7 @@ export const InputCrypto: React.FC = () => {
             ;
         });
     }, [url]);
-
+    console.log(data);
     useEffect(() => {
         const collectionRef = collection(db, "cryptocurrencies")
 
@@ -30,8 +31,18 @@ export const InputCrypto: React.FC = () => {
         });
         return unsubscribe;
     }, [])
+
+
+    cryptos.forEach(crypto => {
+        data.forEach(dat => {
+            if (crypto.symbol == dat.symbol) {
+                crypto.current_price = dat.current_price;
+            }
+        }); 
+    });
+
     const div = <div>
-        {cryptos.map(crypto => user.user.uid === crypto.userId ? <div>{crypto.symbol} {crypto.name} {crypto.value} cena je {crypto.value * data.current_price}</div> : <div></div>)}
+        {user ? (cryptos.map(crypto => user.user.uid === crypto.userId ? <div>{crypto.img} {crypto.name} {crypto.value} cena je {crypto.current_price} Kč</div> : <div></div>)) : <Typography variant="h3">Nejsi přihlášen!</Typography>}
     </div>
 
     return div;
