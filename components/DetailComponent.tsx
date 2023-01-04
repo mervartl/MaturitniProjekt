12efb7a3833f94fd9503678d9d4b4ae7feb10975
mervartl <lukas-mervart@seznam.cz>
2023-01-04 23:53:29 +0100

    detail temp ready

--- components/DetailComponent.tsx
+++ components/DetailComponent.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import axios from "axios";
 import { collection, query, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
@@ -18,18 +18,17 @@ export const DetailComponent : React.FC = ({setDtail, cid}) => {
     const [urlId, setUrlId] = useState("");
 
     const [url, setUrl] = useState("");
-    const [hisUrl, setHisUrl] = useState("");
+    const [hisUrl, setHisUrl] = useState("https://api.coingecko.com/api/v3/coins/markets?vs_currency=czk&order=market_cap_desc&per_page=200&page=1&sparkline=false");
     
     type DBCryptos = { //data z databaze
         map(arg0: (crypto: any) => JSX.Element): import("react").ReactNode;
         forEach(arg0: (crypto: any) => void): unknown;
         id: string;
-        symbol: string;
         name: string;
         img: string;
         value: number;
-        userId: string;
         timestamp: string;
+        nameId: string;
       };
 
     useEffect(() => {
@@ -42,43 +41,71 @@ export const DetailComponent : React.FC = ({setDtail, cid}) => {
     }, [])
 
     useEffect(() => {
-        cryptos.forEach(crypto => {
+        cryptos.forEach(crypto => { //data z db
             if(crypto.id == cid)
             {
                 setName(crypto.name);
-                setUrlId(crypto.name.toLowerCase());
+                setUrlId(crypto.nameId);
                 setImg(crypto.img);
                 setValue(crypto.value);
                 setTstamp(Moment(crypto.timestamp).format('DD-MM-yyyy'));
             }
         });
-
-        setHisUrl(`https://api.coingecko.com/api/v3/coins/${urlId}/history?date=${tstamp}`);
-        setUrl(`https://api.coingecko.com/api/v3/coins/${urlId}?market_data=true&community_data=false&developer_data=false`);
     },[cryptos])
 
     useEffect(() => {
-        axios.get(hisUrl).then((response) => {
-            setHistoData(response.data);
-            
-        });
-    }, [hisUrl]);
+        setHisUrl(`https://api.coingecko.com/api/v3/coins/${urlId}/history?date=${tstamp}`);
+        setUrl(`https://api.coingecko.com/api/v3/coins/${urlId}?market_data=true&community_data=false&developer_data=false`);
+    },[urlId]);
+
+    const regex = /date=(\d+)/;
+    const match = hisUrl.match(regex);
 
     useEffect(() => {
         axios.get(url).then((response) => {
             setCurData(response.data);
-            
         });
-    }, [url]);
-    
-    
-    console.log(curData);
-    console.log(histoData);
+        if(match)
+        {
+            axios.get(hisUrl).then((response) => {
+                setHistoData(response.data);
+            });
+        } 
+    }, [hisUrl]);
+
 
     return(       
         <div>
-            <Button onClick={() => setDtail(false)}>Zpet na seznam</Button>
-            {cid} {name}
+            <Button onClick={() => setDtail(false)}>Zpet na seznam</Button><br/>
+            <Typography>Počet kryptoměny: {value}</Typography><br/>
+            <Grid container columns={2} spacing={1}>
+                <Grid item xs={1} textAlign="center"><Typography variant="h5">Data v době zakoupení</Typography>
+                    <Grid container spacing={2}>
+                        <Grid item>
+
+                        </Grid>
+                        <Grid item>
+
+                        </Grid>
+                        <Grid item>
+
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={1} textAlign="center"><Typography variant="h5">Data dnes</Typography>
+                    <Grid container spacing={2}>
+                        <Grid item>
+
+                        </Grid>
+                        <Grid item>
+
+                        </Grid>
+                        <Grid item>
+
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         </div>
     )
 }
